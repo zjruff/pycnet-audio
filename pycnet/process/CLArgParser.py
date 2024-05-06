9a434db62a4321ca8c7078936f6b204fae0b14ba
Zack Ruff <zjruff@gmail.com>
2024-05-06 16:19:20 -0400

    formatting changes

--- pycnet/process/CLArgParser.py
+++ pycnet/process/CLArgParser.py
@@ -1,4 +1,12 @@
-"""Define behavior of 'pycnet' in relation to command-line arguments."""
+"""Define acceptable command-line arguments when running pycnet as a
+module or through console scripts.
+
+Functions:
+- parsePycnetArgs: create a parser for command-line arguments to pycnet
+- correctPycnetArgs: correct arguments and provide sensible defaults
+(not currently in use)
+
+"""
 
 import argparse
 import multiprocessing as mp
@@ -8,15 +16,22 @@ import os
 def parsePycnetArgs():
     """Define command-line options for the 'pycnet' console script.
     
-    Inevitably messy, so sequestered here instead of in __init__.py
+    Sequestered here for neatness.
+    
+    Arguments:
+    None (reads args from stdin)
+    
+    Returns:
+    - args: an argparse.Namespace object containing command-line 
+    arguments in an accessible form
     """
     n_cores = mp.cpu_count()
 
     parser = argparse.ArgumentParser(description="Perform one or more processing operations on a folder.")
 
     parser.add_argument("mode", metavar="MODE", type=str,
-        choices=["rename", "inventory", "spectro", "predict", "review", "process", "cleanup"], 
-        help="Operation to be performed. Options: 'inventory' (default), 'rename', 'spectro', 'predict', 'review', 'process', 'cleanup', 'config'.", default="inventory")
+        choices=["rename", "inventory", "spectro", "predict", "review", "process", "cleanup", "test"], 
+        help="Operation to be performed. Options: 'inventory' (default), 'rename', 'spectro', 'predict', 'review', 'process', 'cleanup'.", default="inventory")
 
     parser.add_argument("target_dir", metavar="TARGET_DIR", type=str, help="Path to the directory containing .wav files to be processed.")
 
@@ -32,11 +47,11 @@ def parsePycnetArgs():
     parser.add_argument("-r", dest="review_settings", type=str,
         help="Path to file containing settings to use when generating the review file.")
 
-    # parser.add_argument("-o", dest="output_dir", type=str,
-        # help="Path to the directory where output CSV files will be saved. Will be created if it does not already exist. Default: same as target dir.")
+    parser.add_argument("-o", dest="output_file", type=str,
+        help="Manually specify output filename.")
 
-    # parser.add_argument("-q", dest="quiet_mode",
-        # help="Quiet mode (suppress progress bars and informational messages).")
+    parser.add_argument("-q", dest="quiet_mode",
+        help="Quiet mode (suppress progress bars and informational messages).")
 
     # parser.add_argument("-a", dest="auto_cleanup",
         # help="Remove spectrogram image files and temporary folders when class scores have been generated.")
@@ -56,9 +71,16 @@ def correctPycnetArgs(orig_args):
     In almost all cases the user will need to provide a target directory
     containing the audio data (or image data) to be processed.
     
-    Need a dang flowchart for this
+    Think it's more straightforward to accept all the arguments that the
+    user provides and put the relevant ones in an ordered list that will
+    be fed to the appropriate function. The main function of 
+    pycnet.__main__ now does exactly that, rendering this one redundant.
     
     valid modes: rename, inventory, spectro, predict, review, process, cleanup, config
+    
+    Arguments:
+    - orig_args: an argparse Namespace containing the unaltered command
+    line arguments
     
     """
     corrected_args = {}
